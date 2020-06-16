b28914ae54e828c3a56aa061ee9a684458006883
Lucia Brammer <brammerl96@gmail.com>
2020-06-15 17:12:46 -0700

    create bid test and route

---
+++ __tests__/bid.test.js
@@ -0,0 +1,67 @@
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+const Auction = require('../lib/models/Auction');
+const User = require('../lib/models/User');
+const Bid = require('../lib/models/Bid');
+
+describe('auth routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let user;
+  let auction;
+  beforeEach(async() => {
+    user = await User.create({
+      email: `test@test.com`,
+      password: `testpassword`
+    });
+
+    auction = await Auction.create({
+      user: user._id,
+      title: 'auction title',
+      description: 'auction description',
+      quantity: 1,
+      endDate: Date()
+    });
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('creates a bid via POST', () => {
+    return request(app)
+      .post(`/api/v1/bids/`)
+      .send({
+        auction: auction._id,
+        user: user._id,
+        price: 10,
+        quantity: 1,
+        accepted: true
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: 0,
+          _id: expect.anything(),
+          auction: auction.id,
+          user: user.id,
+          price: 10,
+          quantity: 1,
+          accepted: true
+        });
+      });
+  });
+});
